f8ec04b509671324af4ad57efee6aadf0ca2455f
YISUS <jesus.ramirez@gmail.com>
2020-02-18 15:47:22 -0600

    se agrego LOREM en master

--- controllers/NuevoController.php
+++ controllers/NuevoController.php
@@ -9,3 +9,11 @@
 // ESTOS CAMBIOS SON SOBRE UAN RAMA DEL ARBOL LLAMADA NUEVA RAMA 2
 //ESTOS CAMBIOS NO AFECTARAN A LA RAMA PRINCIPAL (ARBOL) LLAMDA MASTER
 //TERMINAMOS DE TRABAJAR EN LA RAMA DESCENDENTE 
+
+
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet odit necessitatibus,
+ consequatur, ducimus maiores expedita optio dolorem repudiandae eaque temporibus 
+ alias corrupti ipsam obcaecati quam ab ea praesentium asperiores! Dolor.
+
+
