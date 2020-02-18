9c9577f7dc71fce908211677f86f42b15d55be75
YISUS <jesus.ramirez@gmail.com>
2020-02-18 15:51:10 -0600

    NOTICIA SOBRE LA UNAM

--- controllers/NuevoController.php
+++ controllers/NuevoController.php
@@ -9,3 +9,11 @@
 // ESTOS CAMBIOS SON SOBRE UAN RAMA DEL ARBOL LLAMADA NUEVA RAMA 2
 //ESTOS CAMBIOS NO AFECTARAN A LA RAMA PRINCIPAL (ARBOL) LLAMDA MASTER
 //TERMINAMOS DE TRABAJAR EN LA RAMA DESCENDENTE 
+
+
+
+ACOSOS EN UNAM 18/02/2020
+
+Alumnos pertenecientes a diferentes planteles de la Universidad Nacional Autónoma de México (UNAM)
+ llegaron a Rectoría en Ciudad Universitaria (CU)para exigir un alto a los casos de acoso 
+ dentro de las aulas de la máxima casa de estudios.
